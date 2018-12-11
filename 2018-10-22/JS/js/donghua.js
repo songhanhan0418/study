dcf6a8d7ab7d427f9231760e3d9713260b7af2af
songhanhan <247297070@qq.com>
2018-12-11 16:05:24 +0800

    12-11

---
+++ 2018-10-22/JS/js/donghua.js
@@ -0,0 +1,53 @@
+function animate(obj,attr,target,isLinear,fn){
+		if(isLinear == undefined){
+			isLinear = true;
+		}
+		var iSpeed = 0;
+		clearInterval(obj.timer);
+		obj.timer = setInterval(function(){
+			var isStop = false;
+			var current = parseFloat(getComputedStyle(obj,false)[attr]);
+			if(attr == 'opacity'){
+				current = Math.round(current *100);
+			}
+			if (isLinear) {
+				if(current > target){
+					iSpeed = -10;
+				}else{
+					iSpeed = 10;
+				}
+				if(Math.abs(target-current) < Math.abs(iSpeed)){
+					if(attr == 'opacity'){
+						obj.style[attr] =target/100;
+					}else{
+						obj.style[attr] =target +'px';
+					}
+					isStop = true;
+				}
+			}else{
+				iSpeed = (target - current)/10;
+				iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+				if(!iSpeed){
+					isStop = true;
+				}
+			}
+			if(isStop){
+				clearInterval(obj.timer);
+				if(typeof fn == 'function'){
+					fn();
+				}
+			}else{
+				if(attr == 'opacity'){
+					obj.style[attr] = (current + iSpeed)/100;
+				}else{
+					obj.style[attr] = current + iSpeed + 'px';
+				}
+			}
+		},30);
+	}
+
+
+
+	function getScrollTop(){
+	return  window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;	
+}
