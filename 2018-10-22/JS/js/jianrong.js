2e80d2e827eb7cf5a03f966842967dfbd2ce8d5f
songhanhan <247297070@qq.com>
2018-12-12 16:31:48 +0800

    12-12

---
+++ 2018-10-22/JS/js/jianrong.js
@@ -0,0 +1,8 @@
+var l = window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;		
+var t = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+	function getScrollTop(){
+		return  window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;	
+	}
+	function getScrollLeft(){
+		return  window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;	
+	}
